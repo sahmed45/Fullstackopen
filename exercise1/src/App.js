426694dc3ec04fdee6e1652d1fd36034935fabda
Syed Ahmed <sahmed45@students.kennesaw.edu>
2021-09-18 19:18:31 -0400

    Exercice 1.3-1.5

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,45 +1,67 @@
-import React from 'react'
-
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
-  )
-}
-const Part = (props) =>{
-  return (
-    <div>
-      <p> {props.part1}</p>
-      <p> {props.part2}</p>
-      <p> {props.part3}</p>
-    </div>
-  )
-}
-
-const Content =() =>{
-  return (
-    <div>
-      <Part part1 = "Fundamentals of React"/>
-      <Part part2 = "Using props to pass data"/>
-      <Part part3 = "State of a component"/>
-    </div>
-  )
-}
-
+import React from "react";
 
 const App = () => {
-  // const-definitions
 
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
   return (
     <div>
-      <Header course= "Half Stack Application Development " />
-      <Content/>
-      </div>
-  )
-}
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};  
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.course.name}</h1>
+		</div>
+	)
+};
 
-export default App
+const Content = (props) => {
+	const list = props.course.parts.map(function(part) {
+		return (
+			<div>
+				<p> {part.name}: {part.exercises}</p>
+			</div>
+		)
+	})
+	
+	return list
+};
+
+const Total = (props) => {
+	var score = 0
+	
+	const list = props.course.parts.map(function(item) {
+		
+		score = score + item.exercises 
+		
+	})
+	
+	return (
+		<div>
+			<p>Total exercises: {score} </p>
+		</div>
+	)
+};
+
+
+export default App;
